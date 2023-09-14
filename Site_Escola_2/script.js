1e7d5a75f0953e8c24cbc4242e0c6029f52d5f04
BreninMilGrau <129687312+BreninMilGrau@users.noreply.github.com>
2023-09-13 21:44:50 -0300

    Add files via upload

---
+++ Site_Escola_2/script.js
@@ -0,0 +1,24 @@
+const carousel = document.getElementById('carousel');
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); // Trocar de slide a cada 5 segundos
+
+showSlide(currentSlide); // Mostrar o slide inicial
+
